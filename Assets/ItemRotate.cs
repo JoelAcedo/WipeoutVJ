9bebf765332f6a4cad11c94381806b35b5793f52
estevevaio <esteve.torrents@est.fib.upc.edu>
2016-12-06 12:56:38 +0100

    added: maxSpeed, turbos, itemBoxes

---
+++ Assets/ItemRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemRotate : MonoBehaviour {
+
+	public float rotSpeed = 60f;
+	private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+		tr = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tr.Rotate (0, rotSpeed * Time.deltaTime, 0, Space.World);
+	}
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float acceleration = 0.01f;
+	public float accelerationF = 0.01f;
+	public float accelerationB = 0.075f;
+	public float maxSpeedF = 15f;
+	public float maxSpeedB = 5f;
 	public float rotation = 1f;
-	private Transform dir;
+	private Transform forward;
+	private Transform backward;
 	private Transform tr;
 	private Rigidbody rb;
 
@@ -13,20 +17,38 @@ public class PlayerMovement : MonoBehaviour {
 	void Start () {
 		tr = gameObject.GetComponent<Transform> ();	
 		rb = gameObject.GetComponent<Rigidbody> ();	
-		dir = GameObject.Find ("Direction").GetComponent<Transform> ();
+		forward = GameObject.Find ("Forward").GetComponent<Transform> ();
+		backward = GameObject.Find ("Backward").GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			rb.velocity += (dir.position - tr.position) * acceleration;
+			if (rb.velocity.magnitude < maxSpeedF) {
+				rb.velocity += (forward.position - tr.position) * accelerationF;
+			} else {
+				//rb.velocity = rb.velocity.normalized * maxSpeedF;
+			}
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			rb.velocity -= (dir.position - tr.position) * acceleration;
+			if (rb.velocity.magnitude < maxSpeedB) {
+				rb.velocity += (backward.position - tr.position) * accelerationB;
+			} else {
+				
+			}
 		} 
+		Debug.Log(rb.velocity.magnitude);
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			tr.eulerAngles = new Vector3 (tr.eulerAngles.x, tr.eulerAngles.y+rotation, tr.eulerAngles.z);
 		} else if (Input.GetKey (KeyCode.LeftArrow)) {
 			tr.eulerAngles = new Vector3 (tr.eulerAngles.x, tr.eulerAngles.y-rotation, tr.eulerAngles.z);
 		} 
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals ("Turbo")) {
+			rb.velocity = rb.velocity.normalized * maxSpeedF * 1.5f;
+		} else if (other.tag.Equals ("Item")) {
+			Destroy (other.gameObject);
+		}
 	}
 }
