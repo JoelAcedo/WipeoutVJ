7c6ddc65c47deb491917fdab403fadd069d2439d
estevevaio <esteve.torrents@est.fib.upc.edu>
2016-12-11 21:05:13 +0100

    added main menu GUI

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onePlayerClicked() {
+		Debug.Log ("onePlayerClicked");
+	}
+
+	void twoPlayersClicked() {
+		Debug.Log ("twoPlayerClicked");
+	}
+
+	void quitClicked() {
+		Application.Quit ();
+	}
+}
